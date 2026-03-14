8181def5e3fe90790dd193c8310774652dc48ee8
Jakub Lewandowski <kubalew.pl@gmail.com>
2026-03-14 22:57:43 +0100

    feat: add a-star algorithm for demons AI

---
+++ include/server/Navigation.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <deque>
+#include <vector>
+
+#include "common/Types.h"
+
+namespace Doom {
+struct Node {
+    Position pos;
+    int g;
+    int h;
+    Position parent;
+
+    int f() const {
+        return g + h;
+    }
+
+    bool operator>(const Node& other) const {
+        return f() > other.f();
+    }
+};
+
+class Navigation {
+   public:
+    static PlayerInput getNextMoveAStar(
+        const Position& start, const Position& target,
+        const std::array<std::array<TileType, BOARD_SIZE>, BOARD_SIZE>& board);
+
+   private:
+    static int manhattanDistance(const Position& a, const Position& b) {
+        return std::abs(a.x - b.x) + std::abs(a.y - b.y);
+    }
+};
+}  // namespace Doom
---
+++ src/server/Navigation.cpp
@@ -0,0 +1,64 @@
+#include "server/Navigation.h"
+
+#include <algorithm>
+#include <map>
+#include <queue>
+#include <set>
+
+namespace Doom {
+PlayerInput Navigation::getNextMoveAStar(
+    const Position& start, const Position& target,
+    const std::array<std::array<TileType, BOARD_SIZE>, BOARD_SIZE>& board) {
+    std::priority_queue<Node, std::vector<Node>, std::greater<Node>> openSet;
+    std::map<Position, int> gScore;
+    std::map<Position, Position> cameFrom;
+
+    openSet.push({start, 0, manhattanDistance(start, target), start});
+    gScore[start] = 0;
+
+    while (!openSet.empty()) {
+        Node current = openSet.top();
+        openSet.pop();
+
+        if (current.pos == target) {
+            Position firstStep = target;
+
+            while (!(cameFrom[firstStep] == start)) {
+                firstStep = cameFrom[firstStep];
+            }
+
+            if (firstStep.x > start.x) return PlayerInput::MoveRight;
+            if (firstStep.x < start.x) return PlayerInput::MoveLeft;
+            if (firstStep.y > start.y) return PlayerInput::MoveDown;
+            if (firstStep.y < start.y) return PlayerInput::MoveUp;
+        }
+
+        const Position possibleNeighbours[4] = {{current.pos.x, current.pos.y - 1},
+                                                {current.pos.x, current.pos.y + 1},
+                                                {current.pos.x - 1, current.pos.y},
+                                                {current.pos.x + 1, current.pos.y}};
+
+        for (const Position neighbour : possibleNeighbours) {
+            if (neighbour.x < 0 || neighbour.y >= BOARD_SIZE || neighbour.y < 0 ||
+                neighbour.y >= BOARD_SIZE)
+                continue;
+            if (board[neighbour.y][neighbour.x] == TileType::Wall) continue;
+
+            int new_gScore = gScore[current.pos] + 1;
+            if (!gScore.count(neighbour) || new_gScore < gScore[neighbour]) {
+                cameFrom[neighbour] = current.pos;
+                gScore[neighbour] = new_gScore;
+                openSet.push(
+                    {neighbour, new_gScore, manhattanDistance(neighbour, target), current.pos});
+            }
+        }
+    }
+
+    return PlayerInput::None;
+};
+
+int Navigation::manhattanDistance(const Position& start, const Position& currentPos) {
+    return std::abs(start.x - currentPos.x) + std::abs(start.y - currentPos.y);
+}
+
+}  // namespace Doom
