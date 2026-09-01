015f9ce4197655932980f8467192be5015b563f5
Jakub Lewandowski <kubalew.pl@gmail.com>
2026-09-01 21:15:43 +0200

    docs: add ui windgets docs

--- include/client/MainMenuWidget.h
+++ include/client/MainMenuWidget.h
@@ -5,14 +5,24 @@
 class QLineEdit;
 
 namespace DoomMan {
+/// Ekran startowy – pozwala podać pseudonim i wybrać tryb rozgrywki.
 class MainMenuWidget : public QWidget {
     Q_OBJECT
    public:
+    /// Tworzy widżet menu głównego.
+    /// @param parent Widżet-rodzic Qt.
     explicit MainMenuWidget(QWidget* parent = nullptr);
 
    signals:
+    /// Emitowany po wybraniu hostowania gry.
+    /// @param nick Pseudonim gracza-hosta.
     void sigHostGame(const QString& nick);
+
+    /// Emitowany po wybraniu dołączenia do gry.
+    /// @param nick Pseudonim dołączającego gracza.
     void sigJoinGame(const QString& nick);
+
+    /// Emitowany po wybraniu wyjścia z programu.
     void sigExit();
 
    private slots:
--- include/client/NetworkManager.h
+++ include/client/NetworkManager.h
@@ -5,14 +5,27 @@
 #include "common/Serialization.h"
 
 namespace DoomMan {
+/// Warstwa sieciowa klienta – utrzymuje połączenie z serwerem,
+/// wysyła komendy ruchu i odbiera aktualizacje stanu gry.
 class ClientNetworkManager : public QObject {
     Q_OBJECT
    public:
+    /// Tworzy menedżera sieciowego klienta.
+    /// @param parent Obiekt-rodzic Qt.
     explicit ClientNetworkManager(QObject* parent = nullptr);
+
+    /// Łączy się z serwerem gry.
+    /// @param host Adres hosta serwera.
+    /// @param port Port TCP serwera.
     void connectToServer(const QString& host, quint16 port);
+
+    /// Wysyła kierunek ruchu gracza do serwera.
+    /// @param input Wciśnięty kierunek ruchu.
     void sendInput(PlayerInput input);
 
    signals:
+    /// Emitowany po odebraniu i zdeserializowaniu stanu gry z serwera.
+    /// @param state Odebrany stan gry.
     void gameStateReceived(const GameState& state);
 
    private slots:
